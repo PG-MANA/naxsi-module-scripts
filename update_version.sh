43689987a45b78dfb682516c430e3cffaa5359d1
PG_MANA <pg_mana@taprix.org>
2024-12-30 17:07:24 +0900

    [fix] update_version.sh

--- update_version.sh
+++ update_version.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
- 
+
+## Before run,
+## Edit nginx-module-naxsi.spec
+## osc add nginx-module-naxsi.spec
+
 cd naxsi
-git checkout $1
+git pull
+git switch --detach $1
 git submodule update
 
 cd ..
 cp -r naxsi nginx-module-naxsi-$1
 rm -rf nginx-module-naxsi-$1/.git*
-tar cvf nginx-module-naxsi-$1.tar.xz nginx-module-naxsi-$1
+tar cvaf nginx-module-naxsi-$1.tar.xz nginx-module-naxsi-$1
 rm -rf nginx-module-naxsi-$1
 
+osc remove *.tar.xz
 osc add nginx-module-naxsi-$1.tar.xz
 osc commit
